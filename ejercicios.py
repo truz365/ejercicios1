4a1420957cd811d55d5ae2d73858883fbc8659f4
truz365 <alvarotrubo@gmail.com>
2022-11-11 10:39:20 +0100

    Ejercicios!

---
+++ ejercicios.py
@@ -0,0 +1,23 @@
+#1- Crea un programa que mostri una llista de les coses que tens en la ment
+llista_random=["agua,", "cafe","python", "acho"];
+print(len(llista_random))
+print(llista_random[0])
+print(llista_random[1])
+print(llista_random[2])
+print(llista_random)
+
+#2-tupla amb els teus videojocs favorits
+tupla=("Lol","valorant","ark")
+print(tupla)
+#3-Diccionari amb les teves dades:
+fitxa_alumen = {
+    "nom": "Alvaro",
+    "edat": 19,
+    "esport preferit": "mugendo",
+    "enemic mortal": "acho"
+}
+print(fitxa_alumen)
+
+#4 set d'habilitats del teu personatge favorit de lol
+myset = { "shuriken","sombra","lanza","explosion"}
+print(myset)
